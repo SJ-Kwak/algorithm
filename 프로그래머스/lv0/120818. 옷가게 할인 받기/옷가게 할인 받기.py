cdc33f6653a8a246c4b31a5a596c9175dd884dce
Seojin Kwak <81500249+SJ-Kwak@users.noreply.github.com>
2023-03-27 23:33:56 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
@@ -0,0 +1,8 @@
+def solution(price):
+    if 100000<=price<300000:
+        price-=price*0.05
+    elif 300000<=price<500000:
+        price-=price*0.1
+    elif price>=500000:
+        price-=price*0.2
+    return int(price)
